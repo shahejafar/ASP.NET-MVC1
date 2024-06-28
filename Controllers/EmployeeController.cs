dd1316ec273e4d61fc8d3839c2f34775eaf236da
shahejafar <shahejafarshaikh@gmail.com>
2024-06-28 12:52:42 +0530

    ASP.NET MVC Project

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPractice.Models;
+using System.Data.Entity;
+namespace MVCPractice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        MVC1DBEntities db = new MVC1DBEntities();
+        public ActionResult Index()
+        {
+            List<tblemp> em = db.tblemps.ToList();
+            return View(em);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tblemp em)
+        {
+            db.tblemps.Add(em);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id) 
+        {
+            tblemp em = db.tblemps.Find(id);
+            return View(em);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tblemp em)
+        {
+            db.Entry<tblemp>(em).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            tblemp em = db.tblemps.Find(id);
+            db.tblemps.Remove(em);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        }
+}
